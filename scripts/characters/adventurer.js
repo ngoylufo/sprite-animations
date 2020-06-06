23b83ba6efcec72b0d86053ce593272b3a47928e
Ngoy Lufo <ngoylufo@gmail.com>
2020-06-06 19:10:53 +0200

    Changed starting x position and added a 'draw-sword' animation.

--- scripts/characters/adventurer.js
+++ scripts/characters/adventurer.js
@@ -1,12 +1,13 @@
 import { AnimatedSprite } from '../modules/sprites.js';
 
 const adventurer = (() => {
-  const attributes = { scale: 3, x: -150, y: 300 };
+  const attributes = { scale: 3, x: 150, y: 300 };
   const source = 'assets/images/adventurer.png';
   return new AnimatedSprite(source, 50, 37, attributes);
 })();
 
 adventurer.addAnimation('idle', ['1:0:3'], { delay: 10 });
+adventurer.addAnimation('draw-sword', ['10:6', '11:0:3'], { delay: 9 });
 adventurer.addAnimation('run', ['2:1:6'], { delay: 5 });
 
 adventurer.onAnimation('run', (sprite) => {
