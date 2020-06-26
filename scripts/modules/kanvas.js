ecc20b33a9927e726ddf0091657c639d0b32e3f2
Ngoy Lufo <ngoylufo@gmail.com>
2020-06-26 03:53:07 +0200

    Added the initialize and setFullscreen functions to the kanvas module.
    - The setFullscreen function sets the widt and height of the given canvas to the
      inner width and height of the window.
    
    - The initialize funtion creates a looper function. It is intended to replace the
      createLooper function and have better features.

--- scripts/modules/kanvas.js
+++ scripts/modules/kanvas.js
@@ -1,5 +1,39 @@
 import { partial } from './utils.js';
 
+function setFullscreen(canvas) {
+  Object.assign(canvas, { width: innerWidth, height: innerHeight });
+}
+
+export function initialize(canvas, options = {}) {
+  const context = canvas.getContext('2d');
+
+  setFullscreen(context.canvas);
+
+  window.addEventListener('resize', () => {
+    setFullscreen(context.canvas);
+  });
+
+  return function looper(callback) {
+    const metrics = { start: 0, timestamp: 0, frames: {} };
+
+    function updateMetrics(metrics, timestamp) {
+      const currFrame = Math.round(timestamp - metrics.start);
+      const { total = 0 } = metrics.frames;
+      const dropped = currFrame - (total + 1);
+
+      metrics.timestamp = timestamp;
+      metrics.frames = { dropped, frame: currFrame, total: total + 1 };
+
+      return metrics;
+    }
+
+    return function loop(timestamp = 0) {
+      requestAnimationFrame(loop);
+      callback(context, updateMetrics(metrics, timestamp));
+    };
+  };
+}
+
 export function createLooper(canvas) {
   const context = canvas.getContext('2d');
   const attributes = { width: window.innerWidth, height: window.innerHeight };
