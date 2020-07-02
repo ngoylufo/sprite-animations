d6da7581768c90c9af63ee48befc7713ff279d8a
Ngoy Lufo <ngoylufo@gmail.com>
2020-07-02 13:13:43 +0200

    Rewrote the adventurer.js script (module?) to use the new Actor class.

--- scripts/characters/adventurer.js
+++ scripts/characters/adventurer.js
@@ -1,10 +1,31 @@
+import Actor from '../modules/actor.js';
+import * as vector from '../modules/vector.js';
 import { AnimatedSprite } from '../modules/sprites.js';
 
-const adventurer = (() => {
-  const attributes = { scale: 3, x: 150, y: 300 };
-  const source = 'assets/images/adventurer.png';
-  return new AnimatedSprite(source, 50, 37, attributes);
+const adventurer = (function () {
+  const filename = 'assets/images/adventurer.png';
+  const sprite = new AnimatedSprite(filename, { width: 32, height: 32 });
+  return new Actor(sprite, 10, [150, -110]);
 })();
+
+adventurer.setInitialState({
+  direction: 1,
+});
+
+adventurer.setUpdate(function(world) {
+  const { direction: d } = this.state;
+
+  if (vector.mag(this.velocity) > 1) {
+    const [vx, vy] = this.velocity;
+    this.velocity = vector.vector(vx ? vx * 0.95 : vx, vy ? vy * 0.15 : vy);
+    this.velocity = vector.limitComponents(this.velocity, [d * 5, 15]);
+  } else {
+    this.velocity = vector.vector(0, 0);
+  }
+
+  this.position = vector.add(this.position, this.velocity);
+});
+
 
 adventurer.addAnimation('idle', ['1:0:3'], { delay: 10 });
 adventurer.addAnimation('draw-sword', ['10:6', '11:0:3'], { delay: 9 });
