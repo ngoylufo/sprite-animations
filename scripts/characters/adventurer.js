d736745865b92c6c30d1ecbbf283f82be299fbd6
Ngoy Lufo <ngoylufo@gmail.com>
2020-07-03 22:15:53 +0200

    Modified the sprites scale and fixed the playAnimation call.

--- scripts/characters/adventurer.js
+++ scripts/characters/adventurer.js
@@ -4,21 +4,25 @@ import { AnimatedSprite } from '../modules/sprites.js';
 
 const adventurer = (function () {
   const filename = 'assets/images/adventurer.png';
-  const sprite = new AnimatedSprite(filename, { width: 32, height: 32 });
-  return new Actor(sprite, 10, [150, -110]);
+  const sprite = new AnimatedSprite(filename, {
+    width: 50,
+    height: 37,
+    scale: 1.5,
+  });
+  return new Actor(sprite, 40, [150, -110]);
 })();
 
 adventurer.setInitialState({
   direction: 1,
 });
 
-adventurer.setUpdate(function(world) {
+adventurer.setUpdate(function (world) {
   const { direction: d } = this.state;
 
   if (vector.mag(this.velocity) > 1) {
     const [vx, vy] = this.velocity;
-    this.velocity = vector.vector(vx ? vx * 0.95 : vx, vy ? vy * 0.15 : vy);
-    this.velocity = vector.limitComponents(this.velocity, [d * 5, 15]);
+    this.velocity = vector.vector(vx ? vx * 0.8 : vx, vy ? vy * 0.15 : vy);
+    this.velocity = vector.limitComponents(this.velocity, [d * 5, 12]);
   } else {
     this.velocity = vector.vector(0, 0);
   }
@@ -26,15 +30,14 @@ adventurer.setUpdate(function(world) {
   this.position = vector.add(this.position, this.velocity);
 });
 
+adventurer.addAnimation('idle', ['0:0:3'], { delay: 10 });
+adventurer.addAnimation('draw-sword', ['9:6', '10:0:3'], { delay: 9 });
+adventurer.addAnimation('run', ['1:1:6'], { delay: 5 });
 
-adventurer.addAnimation('idle', ['1:0:3'], { delay: 10 });
-adventurer.addAnimation('draw-sword', ['10:6', '11:0:3'], { delay: 9 });
-adventurer.addAnimation('run', ['2:1:6'], { delay: 5 });
+adventurer.onAnimation('run', () => {
+  adventurer.apply(vector.vector(1, 0));
+})
 
-adventurer.onAnimation('run', (sprite) => {
-  sprite.move([2.75, 0]);
-});
-
-adventurer.animations.play('idle');
+adventurer.playAnimation('idle');
 
 export default adventurer;
